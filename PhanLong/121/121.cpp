ea50509b76eab9a8039b4ea368c32251020e406d
Phan Long <phanmainhatlong@outlook.com>
2025-07-04 10:28:41 +0700

    121

---
+++ PhanLong/121/121.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int Tinh(int n);
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << Tinh(n);
+    return 0;
+}
+
+int Tinh(int n)
+{
+    int att = 1;
+    int at = 1;
+    int i = 2;
+    int ahh;
+    while (i <= n)
+    {
+        ahh = at + att;
+        i = i + 1;
+        att = at;
+        at = ahh;
+    }
+    return ahh;
+}
