9b2ec6f2e178f4f83de4f72ff1088d0ba7b35a16
AnhGam <guardalex8@gmail.com>
2025-07-04 10:10:50 +0700

    044

---
+++ MA/044/044.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+float tongNghichDao4(int n);
+
+int main() {
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	float s = tongNghichDao4(n);
+	cout << "s =" << s << endl;
+	cout << "\n\n\n Ket thuc !!!!!\n\n\n";
+	return 0;
+}
+
+float tongNghichDao4(int n) {
+	float s = 0;
+	for (int i = 1; i <= 2 * n + 1; i += 2) {
+		s += 1.0f / (i * (i + 1) * (i + 2) * (i + 3));
+	}
+	return s;
+}
