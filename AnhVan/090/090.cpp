4e70ba3a695eae579bc0a95bc11d0bf6846daea7
anhvansan <anhvantran.0910@gmail.com>
2025-07-04 11:29:38 +0700

    090

--- AnhVan/090/090.cpp
+++ AnhVan/090/090.cpp
@@ -13,13 +13,13 @@ int main()
 	cout << "S(x,n) = " << Tinh(x, n);
 }
 
-float Tinh(float x, int n)
+float Tinh(float xx, int nn)
 {
 	float s = 0, t = 1, m = 1;
 	int dau = -1;
-	for (int i = 1;i <= n;i++)
+	for (int i = 1;i <= nn;i++)
 	{
-		t = t * x;
+		t = t * xx;
 		m = m * i;
 		s = s + dau * t / m;
 		dau = -dau;
