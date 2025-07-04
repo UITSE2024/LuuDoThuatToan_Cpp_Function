345dd928a96da26d1be0eca9e0f2698a92e9a1a7
Hoang Duc Chinh <hoangducchinh730@gmail.com>
2025-07-04 11:37:19 +0700

    Toi ngu qua

--- Chinh/118/118.cpp
+++ Chinh/118/118.cpp
@@ -12,11 +12,11 @@ int main()
 	cout << "So hang thu " << n << " cua day: " << Tim(n) << endl;
 }
 
-float Tim(int n)
+float Tim(int nn)
 {
 	float at = 2;
 	int i = 2;
-	while (i <= n)
+	while (i <= nn)
 	{
 		float ahh = (-9 * at - 24) / (5 * at + 13);
 		i++;
