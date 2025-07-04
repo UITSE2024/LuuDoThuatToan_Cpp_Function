040bdb0d46bed3641e37df5bfcd63d18500bc841
Hoang Duc Chinh <hoangducchinh730@gmail.com>
2025-07-04 11:02:10 +0700

    Chinh lam bai 118

---
+++ Chinh/118/118.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+float Tim(int);
+
+int main()
+{
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+
+	cout << "So hang thu " << n << " cua day: " << Tim(n) << endl;
+}
+
+float Tim(int n)
+{
+	float at = 2;
+	int i = 2;
+	while (i <= n)
+	{
+		float ahh = (-9 * at - 24) / (5 * at + 13);
+		i++;
+		at = ahh;
+	}
+	return at;
+}
