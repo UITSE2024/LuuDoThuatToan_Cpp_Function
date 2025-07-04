83ced99d977e21fe4e672a8dc054f12f5100c478
LeonLH928 <lehuynhanhtuan123@gmail.com>
2025-07-04 10:00:56 +0700

    Add 003

---
+++ Tuan/003/003.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+float TinhChuVi(float);
+
+int main()
+{
+	float r;
+	cout << "Nhap r = ";
+	cin >> r;
+
+	cout << "Chu vi duong tron: " << TinhChuVi(r);
+
+	return 0;
+}
+
+float TinhChuVi(float r)
+{
+	return 2 * 3.14 * r;
+}
