1c6f3f0e365f4f48a2886bd217a9871ddf2d4b34
AnhGam <guardalex8@gmail.com>
2025-07-04 10:08:31 +0700

    005

---
+++ MA/005/005.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+float TheTichHinhCau(float r);
+
+int main() {
+	float r;
+	cout << "Nhap ban kinh hinh cau: ";
+	cin >> r;
+	float v = TheTichHinhCau(r);
+	cout << "The tich hinh cau la: " << v << endl;
+	cout << "\n\n\n Ket thuc !!!!!\n\n\n";
+	return 0;
+}
+
+float TheTichHinhCau(float r) {
+	return (4.0f / 3.0f) * 3.14f * r * r * r;
+}
