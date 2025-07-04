f353949782e44c2b57b5f736460c39321b21f942
anhvansan <anhvantran.0910@gmail.com>
2025-07-04 10:59:04 +0700

    142

---
+++ AnhVan/142/142.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int SoDaoNguoc(int);
+
+int main()
+{
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	cout << "So dao nguoc cua n: " << SoDaoNguoc(n);
+	return 0;
+}
+
+int SoDaoNguoc(int n)
+{
+	int dn = 0, t = n;
+	while (t)
+	{
+		int dv = t % 10;
+		dn = dn * 10 + dv;
+		t = t / 10;
+	}
+	return dn;
+}
