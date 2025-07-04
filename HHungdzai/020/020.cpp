7602cc7eb9f25b57b7081532e31599ca37cda27a
ryan <phanhuynhhung0707@gmail.com>
2025-07-04 10:04:19 +0700

    2. 020 done

---
+++ HHungdzai/020/020.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+float tinhX14(float);
+
+int main()
+{
+
+	float x;
+	cout << "Nhap vao gia tri cua x: "; cin >> x;
+
+	float x14 = tinhX14(x);
+
+	cout << "Gia tri cua x14: " << x14;
+
+	cout << "\n\n\nKet thuc!!!!!\n\n\n";
+	return 0;
+}
+
+float tinhX14(float x)
+{
+	float x2 = x * x;
+	float x4 = x2 * x2;
+	float x6 = x4 * x2;
+	float x8 = x4 * x4;
+	float x14 = x8 * x6;
+
+	return x14;
+}
