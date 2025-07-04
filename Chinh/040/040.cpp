3e411a0b30e75c13fe36e47c320192b3e54e7cef
Hoang Duc Chinh <hoangducchinh730@gmail.com>
2025-07-04 11:34:11 +0700

    Toi bi ngu

--- Chinh/040/040.cpp
+++ Chinh/040/040.cpp
@@ -12,11 +12,11 @@ int main()
 	cout << "Gia tri bieu thuc: " << Tong(n) << endl;
 }
 
-int Tong(int n)
+int Tong(int nn)
 {
 	int s = 0;
 	int i = 1;
-	while (i <= n)
+	while (i <= nn)
 	{
 		s = s + i * (i + 1);
 		i++;
