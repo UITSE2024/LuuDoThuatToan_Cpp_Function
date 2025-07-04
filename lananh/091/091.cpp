6a2c6d389fadb06f06563f8da9364e0b95154dfd
ngnlananh <lanh111106@gmail.com>
2025-07-04 11:09:42 +0700

    091

--- lananh/091/091.cpp
+++ lananh/091/091.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-float tinhS(float x, int n)
+float tinhS(float xx, int nn)
 {
 	float s = -1;
 	int t = 1;
 	int m = 1;
 	int i = 2;
 	int dau = 1;
-	while (i <= 2 * n)
+	while (i <= 2 * nn)
 	{
-		t = t * x * x;
+		t = t * xx * xx;
 		m = m * i * (i - 1);
 		s = s + dau * t / m;
 		i = i + 2;
