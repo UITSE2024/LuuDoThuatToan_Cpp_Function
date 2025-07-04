543683e14f73723f1bec3a77686cdecb8cfed16a
AnhGam <guardalex8@gmail.com>
2025-07-04 10:24:33 +0700

    083

---
+++ MA/083/083.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float tongSinLuyThua(int x, int n);
+
+int main() {
+	float x;
+	cout << " Nhap x: ";
+	cin >> x;
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	float s = tongSinLuyThua(x, n);
+	cout << s << endl;
+	cout << "\n\n\n Ket thuc !!!!!\n\n\n";
+	return 0;
+}
+
+float tongSinLuyThua(int x, int n) {
+	float s = 0;
+	long long t = 1;
+	for (int i = 1; i <= n; i++) {
+		t *= x;
+		s += sin(t);
+	}
+	return s;
+}
