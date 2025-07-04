8e9232559df477db253a45b561b876a563cece94
Phan Long <phanmainhatlong@outlook.com>
2025-07-04 10:32:44 +0700

    147

---
+++ PhanLong/147/147.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+bool KiemTraToanLe(int n);
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    if (KiemTraToanLe(n))
+        cout << "Toan le";
+    else
+        cout << "Khong toan le";
+
+    return 0;
+}
+
+bool KiemTraToanLe(int n)
+{
+    int flag = 1;
+    int  t = n;
+    
+    while (t != 0)
+    {
+        int dv = t % 10;
+        if (dv % 2 == 0)
+            flag = 0;
+        t = t / 10;
+    }
+    
+    if (flag == 1)
+        return true;
+    else
+        return false;
+}
