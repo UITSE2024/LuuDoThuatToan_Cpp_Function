7fde1e90b178f049e1377ed31c9ae239f0b05515
QusyPlus <duongnguyenphuquy17052000@gmail.com>
2025-07-04 10:34:22 +0700

    097

---
+++ Quy/097/097.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float Tong(float, int);
+
+int main()
+{
+	int x, n;
+	cout << "Nhap x: ";
+	cin >> x;
+	cout << "Nhap n: ";
+	cin >> n;
+
+	cout << "Tong la: " << Tong(x, n);
+	return 0;
+}
+
+float Tong(float xx, int nn)
+{
+	float s = 0;
+	float t = 1;
+	int i = 1;
+	while (i <= nn)
+	{
+		t = t * xx;
+		s = pow(t + s, 0.5);
+		i = i + 1;
+	}
+	return s;
+}
