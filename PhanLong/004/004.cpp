4c8069fb61bdc45c53fa05e8a0e42ea12438979f
Phan Long <phanmainhatlong@outlook.com>
2025-07-04 10:00:50 +0700

    004

---
+++ PhanLong/004/004.cpp
@@ -0,0 +1,21 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float TinhDienTichXungQuanh(float);
+
+int main()
+{
+    float r;
+    cin >> r;
+
+    float s = TinhDienTichXungQuanh(r);
+    cout << s;
+}
+
+float TinhDienTichXungQuanh(float r)
+{
+    return 4 * M_PI * r * r;
+}
